e346f9ac522caa2a21a462dbfb5465893a1c9305
Nikita <nik1004@gmail.com>
2019-11-15 17:03:47 +0300

    codestyle

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-define(['jquery', 'underscore', 'twigjs'], function ($, _, Twig) {
+define(['jquery', 'underscore', 'twigjs'], function ($, _, twigjs) {
   var CustomWidget = function () {
     var self = this;
 
     this.getTemplate = _.bind(function (template, params, callback) {
-      params = (typeof params == 'object') ? params : {};
+      params = typeof params == 'object' ? params : {};
       template = template || '';
 
       return this.render({
@@ -17,54 +17,63 @@ define(['jquery', 'underscore', 'twigjs'], function ($, _, Twig) {
     this.callbacks = {
       render: function () {
         console.log('render');
+
         return true;
       },
+
       init: _.bind(function () {
         console.log('init');
 
         AMOCRM.addNotificationCallback(self.get_settings().widget_code, function (data) {
-          console.log(data)
+          console.log(data);
         });
 
-        this.add_action("phone", function (params) {
+        this.add_action('phone', function (params) {
           /**
            * код взаимодействия с виджетом телефонии
            */
-          console.log(params)
+          console.log(params);
         });
 
-        this.add_source("sms", function (params) {
+        this.add_source('sms', function (params) {
           /**
            params - это объект в котором будут  необходимые параметры для отправки смс
 
            {
-             "phone": 75555555555,   // телефон получателя
-             "message": "sms text",  // сообщение для отправки
-             "contact_id": 12345     // идентификатор контакта, к которому привязан номер телефона
+             // телефон получателя
+             "phone": 75555555555,
+             // сообщение для отправки
+             "message": "sms text",
+             // идентификатор контакта, к которому привязан номер телефона
+             "contact_id": 12345
           }
            */
 
           return new Promise(_.bind(function (resolve, reject) {
-              // тут будет описываться логика для отправки смс
-              self.crm_post(
-                'https://example.com/',
-                params,
-                function (msg) {
-                  console.log(msg);
-                  resolve();
-                },
-                'text'
-              );
-            }, this)
+            // тут будет описываться логика для отправки смс
+            self.crm_post(
+              'https://example.com/',
+              params,
+              // eslint-disable-next-line max-nested-callbacks
+              function (msg) {
+                console.log(msg);
+                resolve();
+              },
+              'text'
+            );
+          }, this)
           );
         });
 
         return true;
       }, this),
+
       bind_actions: function () {
         console.log('bind_actions');
+
         return true;
       },
+
       settings: _.bind(function ($modal_body) {
         this.getTemplate(
           'oferta',
@@ -81,14 +90,17 @@ define(['jquery', 'underscore', 'twigjs'], function ($, _, Twig) {
               ); // отрисовываем шаблон и добавляем в блок настроек виджета
             $modal_body.find('input[name="oferta_check"]').on('change', function (e) {
               var $checkbox = $(e.currentTarget);
+
               if ($checkbox.prop('checked')) {
-                $modal_body.find('input[name="oferta"]').val('1'); //заполняем поле oferta, если чекбокс отмечен
+                $modal_body.find('input[name="oferta"]').val('1'); // заполняем поле oferta, если чекбокс отмечен
                 $oferta_error.addClass('hidden'); // скрываем предупреждение, если оно отображено
               } else {
                 $modal_body.find('input[name="oferta"]').val(''); // очищаем поле oferta, если не отмечен чекбокс
               }
             });
-            //при нажатии на кнопку "Установить", если не отмечен чекбокс, отображаем предупреждение
+
+            // при нажатии на кнопку "Установить", если не отмечен чекбокс,
+            // отображаем предупреждение
             $install_btn.on('click', function () {
               if (!$modal_body.find('input[name="oferta"]').val()) {
                 $oferta_error.removeClass('hidden');
@@ -99,62 +111,69 @@ define(['jquery', 'underscore', 'twigjs'], function ($, _, Twig) {
 
         return true;
       }, this),
+
       onSave: function () {
-        alert('click');
+        console.log('click');
+
         return true;
       },
-      destroy: function () {
 
+      destroy: function () {
+        _.noop();
       },
+
       contacts: {
-        //select contacts in list and clicked on widget name
+        // select contacts in list and clicked on widget name
         selected: function () {
           console.log('contacts');
         }
       },
+
       leads: {
-        //select leads in list and clicked on widget name
+        // select leads in list and clicked on widget name
         selected: function () {
           console.log('leads');
         }
       },
+
       tasks: {
-        //select taks in list and clicked on widget name
+        // select taks in list and clicked on widget name
         selected: function () {
           console.log('tasks');
         }
       },
+
       advancedSettings: _.bind(function () {
         var $work_area = $('#work-area-' + self.get_settings().widget_code),
-          $save_button = $(
-            Twig({ref: '/tmpl/controls/button.twig'}).render({
-              text: 'Сохранить',
-              class_name: 'button-input_blue button-input-disabled js-button-save-' + self.get_settings().widget_code,
-              additional_data: ''
-            })
-          ),
-          $cancel_button = $(
-            Twig({ref: '/tmpl/controls/cancel_button.twig'}).render({
-              text: 'Отмена',
-              class_name: 'button-input-disabled js-button-cancel-' + self.get_settings().widget_code,
-              additional_data: ''
-            })
-          );
+            $save_button = $(
+              twigjs({ ref: '/tmpl/controls/button.twig' }).render({
+                text: 'Сохранить',
+                class_name: 'button-input_blue button-input-disabled js-button-save-' + self.get_settings().widget_code,
+                additional_data: ''
+              })
+            ),
+            $cancel_button = $(
+              twigjs({ ref: '/tmpl/controls/cancel_button.twig' }).render({
+                text: 'Отмена',
+                class_name: 'button-input-disabled js-button-cancel-' + self.get_settings().widget_code,
+                additional_data: ''
+              })
+            );
 
         console.log('advancedSettings');
 
         $save_button.prop('disabled', true);
-        $('.content__top__preset').css({float: 'left'});
+        $('.content__top__preset').css({ float: 'left' });
 
-        $('.list__body-right__top').css({display: 'block'})
+        $('.list__body-right__top').css({ display: 'block' })
           .append('<div class="list__body-right__top__buttons"></div>');
-        $('.list__body-right__top__buttons').css({float: 'right'})
+        $('.list__body-right__top__buttons').css({ float: 'right' })
           .append($cancel_button)
           .append($save_button);
 
         self.getTemplate('advanced_settings', {}, function (template) {
           var $page = $(
-            template.render({title: self.i18n('advanced').title, widget_code: self.get_settings().widget_code})
+            template.render({ title: self.i18n('advanced').title, widget_code: self.get_settings().widget_code })
           );
 
           $work_area.append($page);
@@ -162,10 +181,12 @@ define(['jquery', 'underscore', 'twigjs'], function ($, _, Twig) {
       }, self),
 
       /**
-       * Метод срабатывает, когда пользователь в конструкторе Salesbot размещает один из хендлеров виджета.
+       * Метод срабатывает, когда пользователь в конструкторе
+       * Salesbot размещает один из хендлеров виджета.
        * Мы должны вернуть JSON код salesbot'а
        *
-       * @param handler_code - Код хендлера, который мы предоставляем. Описан в manifest.json, в примере равен handler_code
+       * @param handler_code - Код хендлера, который мы предоставляем.
+       * Описан в manifest.json, в примере равен handler_code
        * @param params - Передаются настройки виджета. Формат такой:
        * {
        *   button_title: "TEST",
@@ -179,23 +200,23 @@ define(['jquery', 'underscore', 'twigjs'], function ($, _, Twig) {
        */
       onSalesbotDesignerSave: function (handler_code, params) {
         var salesbot_source = {
-            question: [],
-            require: []
-          },
-          button_caption = params.button_caption || "",
-          button_title = params.button_title || "",
-          text = params.text || "",
-          number = params.number || 0,
-          handler_template = {
-            handler: "show",
-            params: {
-              type: "buttons",
-              value: text + ' ' + number,
-              buttons: [
-                button_title + ' ' + button_caption,
-              ]
-            }
-          };
+              question: [],
+              require: []
+            },
+            button_caption = params.button_caption || '',
+            button_title = params.button_title || '',
+            text = params.text || '',
+            number = params.number || 0,
+            handler_template = {
+              handler: 'show',
+              params: {
+                type: 'buttons',
+                value: text + ' ' + number,
+                buttons: [
+                  button_title + ' ' + button_caption,
+                ]
+              }
+            };
 
         console.log(params);
 
@@ -204,6 +225,7 @@ define(['jquery', 'underscore', 'twigjs'], function ($, _, Twig) {
         return JSON.stringify([salesbot_source]);
       },
     };
+
     return this;
   };
 
